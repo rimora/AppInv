7503860dcd20c285b2cf2b4b784e44d2978512f0
rimora <rimora7@hotmail.com>
2019-10-17 01:32:14 -0500

    productiva
    productiva

--- js/eventos.js
+++ js/eventos.js
@@ -42,8 +42,8 @@ $('#BtnIngresa').click(function () {
       //alert(articulo);
      
                           $.ajax({
-                          //  url: "http://10.0.0.202:7010/api/articulos/"+ articulo,
-                            url: "http://192.168.0.102:7010/api/articulos/"+ articulo,
+                            url: "http://10.0.0.202:7010/api/articulos/"+ articulo,
+                         //   url: "http://192.168.0.102:7010/api/articulos/"+ articulo,
                             type: "GET",
                             dataType: 'json',
                            // data:{dispositivo:dispositivo,tipooperacion:operacion,devcab:detalles},
@@ -103,8 +103,8 @@ $('#BtnGuardar').click(function () {
             
             
             
-            //$.ajax({type: "POST",  url: "http://10.0.0.202:7010/api/articulos/", contentType: "application/json; charset=utf-8",
-            $.ajax({type: "POST",  url: "http://192.168.0.102:7010/api/articulos/", contentType: "application/json; charset=utf-8",
+            $.ajax({type: "POST",  url: "http://10.0.0.202:7010/api/articulos/", contentType: "application/json; charset=utf-8",
+           // $.ajax({type: "POST",  url: "http://192.168.0.102:7010/api/articulos/", contentType: "application/json; charset=utf-8",
                 dataType: "json", data:t}).done(function( filasafectadas ) {  
                
                 if (filasafectadas>0)
