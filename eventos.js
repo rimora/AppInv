fe5707a409422c790f01c26d8bb3c24c9e6f7326
rimora <rimora7@hotmail.com>
2021-01-09 04:32:07 -0600

    borrarlo

--- eventos.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    document.addEventListener("deviceready",function(){ 
-
-
-        navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
-
-   
- },false);//document.addEventListener("deviceready",function(){    
-});//$(document).ready(function() 
