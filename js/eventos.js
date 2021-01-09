d9d2d2e0cb88c68173a76cbce84d1962c13c741e
rimora <rimora7@hotmail.com>
2021-01-09 17:29:56 -0600

    Add files via upload

--- js/eventos.js
+++ js/eventos.js
@@ -1,31 +1,52 @@
 $(document).ready(function() {
 	var existearticulo=0;
-var json = {"alumnoUTP":[{"nombre":"Ricardo","apePaterno":"Carpio","edad":39},{"nombre":"Thiago","apePaterno":"Carpio","edad":5},{"nombre":"José","apePaterno":"Carpio","edad":74}]};
+
 
     document.addEventListener("deviceready",function(){ 
 
-$('#BtnIngresa').click(function () {  
-      window.location.href='#pagina2';
-	  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
- });
- $('#pagina2').live('pagebeforeshow',function(event, ui){
-        navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
-	var DatosJson = JSON.parse(JSON.stringify(json));
-    //console.log(DatosJson.alumnoUTP.length);
-    $("#tabla").append('<tr><td>Nombre</td>'+
- 	'<td>Apellido paterno</td>' + 
- 	'<td>Edad</td>');
-    for (i = 0; i < DatosJson.alumnoUTP.length; i++){
- 
-		$("#tabla").append('<tr>' + 
-		'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].nombre + '</td>'+
-		'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].apePaterno + '</td>'+
-		'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].edad + '</td>'+'</tr>');
-	}
- });
 
 
    
- },false);//document.addEventListener("deviceready",function(){    
- 
+ },false);//document.addEventListener("deviceready",function(){  
+function DatosJ(estado){ 			  		      
+		var json = {"Pedidos":[{"pedido":"P001","cliente":"Carpio","estado":"pendiente"},{"pedido":"P002","cliente":"Ricardo","estado":"pendiente"},{"pedido":"P003","cliente":"Carpio","estado":"entregado"},{"pedido":"P004","cliente":"Carpio","estado":"entregado"},{"pedido":"P005","cliente":"Carpio","estado":"pendiente"}]};
+		var DatosJson = JSON.parse(JSON.stringify(json));
+		$("#tabla").empty();
+        $("#tabla").append('<tr><td>Pedido</td>'+
+		'<td>Cliente</td>' + 
+		'<td>Estado</td>');
+		for (i = 0; i < DatosJson.Pedidos.length; i++){			
+			if ((estado==DatosJson.Pedidos[i].estado) || (estado=="todos"))
+			{
+				$("#tabla").append('<tr>' + 
+				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].pedido + '</td>'+
+				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].cliente + '</td>'+
+				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].estado + '</td>'+'</tr>');
+			}//if			
+		}//for
+}//function DatosJ
+ $('#BtnIngresa').click(function () {  
+      window.location.href='#pagina2';
+	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
+ });
+ $('#pagina2').on('pagebeforeshow',function(event, ui){
+	 //alert('hola');
+	 DatosJ("pendiente");
+     //   navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
+	
+ });
+$("#radio_pendientes").bind( "change", function(event, ui) {	
+  //alert($("#radio_pendientes").val());
+  
+	DatosJ("pendiente");   
+});
+$("#radio_entregados").bind( "change", function(event, ui) {	
+  //alert($("#radio_pendientes").val());  
+	DatosJ("entregado");    
+});
+$("#radio_todos").bind( "change", function(event, ui) {	
+  //alert($("#radio_pendientes").val());
+  
+	DatosJ("todos");
+});
 });//$(document).ready(function() 
