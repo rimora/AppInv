3e04fc6a74deda90baf4c227049ac0ee0aa1e4df
rimora <rimora7@hotmail.com>
2021-01-19 01:23:33 -0600

    Add files via upload

--- js/eventos.js
+++ js/eventos.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-	var existearticulo=0;
-	 var l = localStorage;
-	 var jsontodospedidos;
-	 
+	 var existearticulo=0;
+	 var l = localStorage;	 
+	 var pedidoactual;
 
     document.addEventListener("deviceready",function(){ 
 
@@ -10,109 +9,94 @@ $(document).ready(function() {
 
    
  },false);//document.addEventListener("deviceready",function(){  
-/*
-	function DatosJ(estado){ 			  		      
-		var json = {"Pedidos":[{"pedido":"P001","cliente":"Carpio","estado":"pendiente"},{"pedido":"P002","cliente":"Ricardo","estado":"pendiente"},{"pedido":"P003","cliente":"Carpio","estado":"entregado"},{"pedido":"P004","cliente":"Carpio","estado":"entregado"},{"pedido":"P005","cliente":"Carpio","estado":"pendiente"}]};
-		var DatosJson = JSON.parse(JSON.stringify(json));
-		$("#tabla").empty();
-        $("#tabla").append('<tr><td>Pedido</td>'+
-		'<td>Cliente</td>' + 
-		'<td>Estado</td>');
-		for (i = 0; i < DatosJson.Pedidos.length; i++){			
-			if ((estado==DatosJson.Pedidos[i].estado) || (estado=="todos"))
-			{
-				$("#tabla").append('<tr>' + 
-				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].pedido + '</td>'+
-				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].cliente + '</td>'+
-				'<td align="center" style="dislay: none;">' + DatosJson.Pedidos[i].estado + '</td>'+'</tr>');
-			}//if			
-		}//for
-}//function DatosJ*/
-function DatosJ(estado){ 			  		      
-	var DatosJson = JSON.parse(JSON.stringify(jsontodospedidos));
+function CrearGridPedidos(){ 			  		      
 	var html = "";
+	
 	$("#gridpedidos").empty();
 	html+='<div class="ui-block-a" style="width:30%"><div class="ui-bar ui-bar-a"><strong>Pedido</strong></div></div>';
-	html+='<div class="ui-block-b" style="width:50%"><div class="ui-bar ui-bar-b"><strong>Cliente</strong></div></div>';
-	html+='<div class="ui-block-c" style="width:20%"><div class="ui-bar ui-bar-e"><strong>Estado</strong></div></div>';
-	
-    $("#gridpedidos").append('');
-	for (i = 0; i < DatosJson.Pedidos.length; i++){			
-		if ((estado==DatosJson.Pedidos[i].estado) || (estado=="todos"))
-		{
-			html+='<div class="ui-block-a" style="width:30%"><div class="ui-bar ui-bar-a"><strong><a href="#" class="clasepedido" name=" '+DatosJson.Pedidos[i].pedido +'">'+DatosJson.Pedidos[i].pedido +'</a></strong></div></div>';
-			html+='<div class="ui-block-b" style="width:50%"><div class="ui-bar ui-bar-b"><strong>' + DatosJson.Pedidos[i].cliente + '</strong></div></div>';			
-			html+='<div class="ui-block-c" style="width:20%"><div class="ui-bar ui-bar-e"><strong>' + DatosJson.Pedidos[i].estado + '</strong></div></div>';
-		}//if			
-	}//for
- $("#gridpedidos").append(html);
-}//function DatosJ*/	
+	html+='<div class="ui-block-b" style="width:70%"><div class="ui-bar ui-bar-b"><strong>Cliente</strong></div></div>';    
+
+	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
+	  $.ajax({
+                            url: "http://localhost:60481/api/pedidos/",                         
+                            type: "GET",
+                            dataType: 'json',                           
+                            success: function(resultado){
+                            // Se ejecuta cuando se ha recibido correctamente
+                            // los datos de la url
+							
+								var	DatosJson=JSON.parse(JSON.stringify(resultado));								
+								for (i = 0; i < DatosJson.length; i++){		
+									html+='<div class="ui-block-a" style="width:30%"><div class="ui-bar ui-bar-a"><strong><a href="#" class="clasepedido" name="'+DatosJson[i].pedido +'">'+DatosJson[i].pedido +'</a></strong></div></div>';
+									html+='<div class="ui-block-b" style="width:70%"><div class="ui-bar ui-bar-b"><strong>' + DatosJson[i].cliente + '</strong></div></div>';			
+								}//for
+                            },
+                            error: function(jqxhr, textStatus, error){
+                               // $('#DescripcionArticulo').html("Articulo no encontrado en el catálogo");
+                                alert("error");
+                            },
+                            async: false, // La petici�n es s�ncrona
+                            cache: false // No queremos usar la cach� del navegador
+                            });		
+	$("#gridpedidos").append(html); 
+}//function CrearGridPedidos	
 function DatosPedido(jsonpedido){ 			  		      
 	
 	var DatosJson = JSON.parse(JSON.stringify(jsonpedido));
 	var html = "";	
 	var estado="";
 	$("#datospedido").empty();
-	for (i = 0; i < DatosJson.Pedidos.length; i++){		
-		html+='<p>Pedido:'+DatosJson.Pedidos[i].pedido +'  Estado: <b> '+DatosJson.Pedidos[i].estado+'</p>';
-		html+='<p>Cliente:' + DatosJson.Pedidos[i].cliente + '</p>';
-		html+='<p>Dirección:' + DatosJson.Pedidos[i].direccion + '</p>';
-		html+='<p>Contacto:' + DatosJson.Pedidos[i].contacto + '</p>';		
-		html+='<p>Telefeno:' + DatosJson.Pedidos[i].telefono + '</p>';		
-		estado=DatosJson.Pedidos[i].estado;
+	for (i = 0; i < DatosJson.length; i++){		
+		html+='<p>Pedido:'+DatosJson[i].pedido +'</p>';
+		html+='<p>Cliente:' + DatosJson[i].cliente + '</p>';
+		html+='<p>Dirección Cobro:' + DatosJson[i].dircobro + '</p>';
+		html+='<p>Dirección Embarque:' + DatosJson[i].dirembarque + '</p>';
+		html+='<p>Contacto:' + DatosJson[i].contacto + '</p>';		
+		html+='<p>Teléfono:' + DatosJson[i].telefono + '</p>';		
+		pedidoactual=DatosJson[i].pedido;
 	}//for
 $("#datospedido").append(html);
-	alert(estado);
-if (estado=="entregado")
-	{
-		$("#botones").hide();
-	}
-}//function DatosJ*/	
-	
-//EVENTOS Y ELEMENTOS DE PAGINA 1
-	
- $('#BtnIngresa').click(function () {  
-	 jsontodospedidos = {"Pedidos":[{"pedido":"P001","cliente":"Carpio","estado":"pendiente"},{"pedido":"P002","cliente":"Ricardo","estado":"pendiente"},{"pedido":"P003","cliente":"Carpio","estado":"entregado"},{"pedido":"P004","cliente":"Carpio","estado":"entregado"},{"pedido":"P005","cliente":"Carpio","estado":"pendiente"}]};
-	
-      window.location.href='#pagina2';
-	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
- });
 
-	//EVENTOS Y ELEMENTOS DE PAGINA 2
-	
- $('#pagina2').on('pagebeforeshow',function(event, ui){	
-	
-	DatosJ("pendiente");
-     //   navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
+}//function DatosJ	
+ $('#BtnIngresa').click(function () {  
+	CrearGridPedidos();
+	window.location.href='#pagina2';
 	
  });
-$(document).on("click", ".navpedidos", function(){
-	var estado= $(this).attr("name");//el nombre tiene el numero de pedido	
-  	DatosJ(estado);   
-});
 $(document).on("click", "a.clasepedido", function(){//al seleccionar un pedido del grid se debe obtener el detalle del pedido
 	var pedido= $(this).attr("name");//el nombre tiene el numero de pedido
 	
-	//obtiene el detalle del pedido
-	var jsonpedido = {"Pedidos":[{"pedido":pedido,"cliente":"Carpio","estado":"pendiente","direccion":"Direccion","contacto":"datos del contacto","telefono":"telefono"}]};
-	DatosPedido(jsonpedido);    
+	//obtiene el detalle del pedido	
+	$.ajax({
+                            url: "http://localhost:60481/api/pedidos/"+pedido,                         
+                            type: "GET",
+                            dataType: 'json',                           
+                            success: function(resultado){
+                            // Se ejecuta cuando se ha recibido correctamente                            
+								/*
+							alert(JSON.stringify(resultado));
+							jsontodospedidos=JSON.parse(JSON.stringify(resultado));
+							alert(jsontodospedidos[0].pedido);
+							*/
+							DatosPedido(resultado);
+                            },
+                            error: function(jqxhr, textStatus, error){                               
+                                alert("error");
+                            },
+                            async: false, // La petici�n es s�ncrona
+                            cache: false // No queremos usar la cach� del navegador
+                            });	  
 	window.location.href='#pagina3';
 });
-
-	
-	//EVENTOS Y ELEMENTOS DE PAGINA 3
-	
-
 $('#pagina3').on('pagebeforeshow',function(event, ui){
-	
 	
      //   navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
 	$("#textbitacora").val(''); 
-	
+	$("#botones").show();	
 	
  });
  $('#BtnComentario').click(function () {  
-	 var comentario= $('#textbitacora').val(); 
+	 var comentario= $('#textbitacora').val(); 	 
 	 if (comentario=="")
 	 {
 		alert("Debe indicar comentario");
@@ -120,33 +104,68 @@ $('#pagina3').on('pagebeforeshow',function(event, ui){
 	 else
 	 {
 		//guardar comentario en servidor
-		 navigator.notification.alert('Comentario Ingresado',null,'Guardar Comentario','Aceptar');
-		 $("#textbitacora").val(''); 
+		 var objetocomentario =  
+                 {  
+          
+                     ClavePedido: pedidoactual,  
+                     comentario: comentario,
+					 Entregado:false
+                };  
+         
+            var t = JSON.stringify(objetocomentario);  
+           
+            $.ajax({type: "POST",  url: "http://localhost:60481/api/pedidos/", contentType: "application/json; charset=utf-8",           
+                dataType: "json", data:t}).done(function( filasafectadas ) {  
+               
+                if (filasafectadas>0)
+                {
+                  
+                  alert("Comentario insertado");
+				  $("#textbitacora").val(''); 
+                }
+                else
+                {
+                    alert("Ocurrio un error al guardar el comentario, consulte al administrador del sistema");
+                }
+				
+            }); //AJAX  
+
+		 //navigator.notification.alert('Comentario Ingresado',null,'Guardar Comentario','Aceptar');
+		 
 	 }
 		 
 
 
 	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
  });
-$('#BtnEntregar').click(function () {  
-	 alert ("Pedido entregado");
-	$("#botones").hide();
-	jsontodospedidos = {"Pedidos":[{"pedido":"P001","cliente":"Carpio","estado":"pendiente"},{"pedido":"P002","cliente":"Ricardo","estado":"pendiente"},{"pedido":"P003","cliente":"Carpio","estado":"entregado"},{"pedido":"P004","cliente":"Carpio","estado":"entregado"},{"pedido":"P005","cliente":"Carpio","estado":"pendiente"},{"pedido":"P006","cliente":"Carpio","estado":"entregado"}]};
+ $('#BtnEntregar').click(function () {  
+	 var objetocomentario =  
+                 {            
+                     ClavePedido: pedidoactual,  
+                     Entregado: true          
+                };  
+         
+            var t = JSON.stringify(objetocomentario);              
+            $.ajax({type: "POST",  url: "http://localhost:60481/api/pedidos/", contentType: "application/json; charset=utf-8",           
+                dataType: "json", data:t}).done(function( filasafectadas ) {  
+               
+                if (filasafectadas>0)
+                {
+                  
+                  alert("Pedido Actualizado");				  
+				  CrearGridPedidos();
+                }
+                else
+                {
+                    alert("Ocurrio un error al actualizar el pedido,consulte al administrador del sistema");
+                }
+				
+            }); //AJAX  
+	
+	
+	//$("#gridpedidos").empty();
+	$("#botones").hide();	
 	
 	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
  });
-$("a.clasepedido").bind( "click", function(event, ui){//al seleccionar un pedido
-	
-             /*var recibo= $(this).attr("name");//el nombre tiene el numero de recibo en la tabla PENCOBRO
-             var tipoop=window.localStorage.getItem("reimpresion");
-             var bodega=window.localStorage.getItem("bodega");
-             if (tipoop=='R'){//reimpresion
-             imprimircobro(recibo);
-             }
-             else{
-                 cancelarcobro(recibo);
-             }*/
-                 
- }); 
-	
 });//$(document).ready(function() 
