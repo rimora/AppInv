e3e6cdfed66d8e2daee9de7e519a34615df0c785
rimora <rimora7@hotmail.com>
2021-10-11 10:19:53 -0500

    cambioIP

--- js/eventos.js
+++ js/eventos.js
@@ -1,180 +1,155 @@
-$(document).ready(function() {
-	 var existearticulo=0;
-	 var l = localStorage;	 
-	 var pedidoactual;
 
+$(document).ready(function() {
+    
+/*
     document.addEventListener("deviceready",function(){ 
 
+    
+  },false);//document.addEventListener("deviceready",function(){    
 
+*/
+var existearticulo=0;
+$('#BtnIngresa').click(function () {
+  var bodega= $('#selectbodega').val();  
+    if (bodega=="XXX")
+    {
+      alert("Seleccione bodega por favor");
+    }
+    else
+    {
+      window.location.href='#pagina2';      
+    }
+  });
+ $('#txt-articulo').focusout(function() {
+       var articulo= $('#txt-articulo').val();  
+      
+       /*
+       $.ajax({url: "http://189.198.138.158:7010/api/articulos/"+ articulo, success: function(resultado){
+            
+          //alert(resultado[0].Descripcion);
+          
+            $('#DescripcionArticulo').html(resultado[0].Descripcion);
+               existearticulo=1;
+                 
+            },error : function(xhr, status) {        
+              
+              $('#DescripcionArticulo').html("Articulo no encontrado en el catálogo");
+              
+            }
+          });//AJAX
 
-   
- },false);//document.addEventListener("deviceready",function(){  
-function CrearGridPedidos(){ 			  		      
-	var html = "";
-	
-	$("#gridpedidos").empty();
-	html+='<div class="ui-block-a" style="width:30%"><div class="ui-bar ui-bar-a"><strong>Pedido</strong></div></div>';
-	html+='<div class="ui-block-b" style="width:70%"><div class="ui-bar ui-bar-b"><strong>Cliente</strong></div></div>';    
-
-	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
-	  $.ajax({
-                            url: "http://Hergomrdp.ddns.net:7010/api/pedidos/",                         
+*/
+      //alert(articulo);
+     
+                          $.ajax({
+                            url: "http://189.198.138.158:7010/api/articulos/"+ articulo,
+                         //   url: "http://192.168.0.102:7010/api/articulos/"+ articulo,
                             type: "GET",
-                            dataType: 'json',                           
+                            dataType: 'json',
+                           // data:{dispositivo:dispositivo,tipooperacion:operacion,devcab:detalles},
                             success: function(resultado){
                             // Se ejecuta cuando se ha recibido correctamente
                             // los datos de la url
-							
-								var	DatosJson=JSON.parse(JSON.stringify(resultado));								
-								for (i = 0; i < DatosJson.length; i++){		
-									html+='<div class="ui-block-a" style="width:30%"><div class="ui-bar ui-bar-a"><strong><a href="#" class="clasepedido" name="'+DatosJson[i].pedido +'">'+DatosJson[i].pedido +'</a></strong></div></div>';
-									html+='<div class="ui-block-b" style="width:70%"><div class="ui-bar ui-bar-b"><strong>' + DatosJson[i].cliente + '</strong></div></div>';			
-								}//for
+                                $('#DescripcionArticulo').html(resultado[0].Descripcion);
+                                existearticulo=1;     
                             },
                             error: function(jqxhr, textStatus, error){
-                               // $('#DescripcionArticulo').html("Articulo no encontrado en el catálogo");
-                                //navigator.notification.alert('Error al poblar pedidos verifique su conexión al servidor',null,'Poblar Pedidos','Aceptar');
-								alert('Error al poblar pedidos verifique su conexión al servidor');
+                                $('#DescripcionArticulo').html("Articulo no encontrado en el catálogo");
+                                
                             },
                             async: false, // La petici�n es s�ncrona
                             cache: false // No queremos usar la cach� del navegador
-                            });		
-	$("#gridpedidos").append(html); 
-}//function CrearGridPedidos	
-function DatosPedido(jsonpedido){	
-	var DatosJson = JSON.parse(JSON.stringify(jsonpedido));
-	var html = "";	
-	var estado="";
-	$("#datospedido").empty();
-	for (i = 0; i < DatosJson.length; i++){		
-		html+='<p>Pedido:'+DatosJson[i].pedido +'</p>';
-		html+='<p>Cliente:' + DatosJson[i].cliente + '</p>';
-		html+='<p>Dirección Cobro:' + DatosJson[i].dircobro + '</p>';
-		html+='<p>Dirección Embarque:' + DatosJson[i].dirembarque + '</p>';
-		html+='<p>Contacto:' + DatosJson[i].contacto + '</p>';		
-		html+='<p>Teléfono:' + DatosJson[i].telefono + '</p>';		
-		html+='<p>Observaciones:' + DatosJson[i].observaciones + '</p>';		
-		pedidoactual=DatosJson[i].pedido;
-	}//for
-$("#datospedido").append(html);
+                            });
 
-}//function DatosJ	
- $('#BtnIngresa').click(function () {  
-	CrearGridPedidos();
-	window.location.href='#pagina2';
-	
- });
-$(document).on("click", "a.clasepedido", function(){//al seleccionar un pedido del grid se debe obtener el detalle del pedido
-	var pedido= $(this).attr("name");//el nombre tiene el numero de pedido
-	
-	//obtiene el detalle del pedido	
-	$.ajax({
-                            url: "http://Hergomrdp.ddns.net:7010/api/pedidos/"+pedido,                         
-                            type: "GET",
-                            dataType: 'json',                           
-                            success: function(resultado){
-                            // Se ejecuta cuando se ha recibido correctamente                            
-								/*
-							alert(JSON.stringify(resultado));
-							jsontodospedidos=JSON.parse(JSON.stringify(resultado));
-							alert(jsontodospedidos[0].pedido);
-							*/
-							DatosPedido(resultado);
-                            },
-                            error: function(jqxhr, textStatus, error){                               
-                                alert("error");
-                            },
-                            async: false, // La petici�n es s�ncrona
-                            cache: false // No queremos usar la cach� del navegador
-                            });	  
-	window.location.href='#pagina3';
+
+
+/*
+                $.getJSON("http://192.168.1.77:7010/api/articulos/"+ articulo, function(resultado) {
+                  $('#DescripcionArticulo').html(resultado[0].Descripcion);
+                                existearticulo=1;     
+               });
+
+*/
+
+
+
 });
-$('#pagina3').on('pagebeforeshow',function(event, ui){
+
+$('#BtnGuardar').click(function () {
+    var cantidad= $('#num-cantidad').val();  
+    var articulo= $('#txt-articulo').val();  
+    var bodega= $('#selectbodega').val(); 
+    var boleta= $('#txt-boleta').val(); 
 	
-     //   navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
-	$("#textbitacora").val(''); 
-	$("#botones").show();	
-	
- });
- $('#BtnComentario').click(function () {  
-	 var comentario= $('#textbitacora').val(); 	 
-	 if (comentario=="")
-	 {
-		//navigator.notification.alert('Debe indicar comentario',null,'Guardar comentario','Aceptar');
-		alert('Debe indicar comentario');
-	 }
-	 else
-	 {
-		//guardar comentario en servidor
-		 var objetocomentario =  
+       if (cantidad<=0 || existearticulo==0)
+       {
+        alert("La cantidad debe ser mayor a cero y debe existir el articulo en el catalogo");
+       }
+       else
+       {
+         var objetoarticulo =  
                  {  
           
-                     ClavePedido: pedidoactual,  
-                     comentario: comentario,
-					 Entregado:false
+                     clavearticulo: articulo,  
+                     descripcion: "LXI",   
+                     existencia: cantidad,  
+                     bodega: bodega,
+                     boleta: boleta
+          
                 };  
          
-            var t = JSON.stringify(objetocomentario);  
+            var t = JSON.stringify(objetoarticulo);  
            
-            $.ajax({type: "POST",  url: "http://Hergomrdp.ddns.net:7010/api/pedidos/", contentType: "application/json; charset=utf-8",           
+            
+            
+            
+            $.ajax({type: "POST",  url: "http://189.198.138.158:7010/api/articulos/", contentType: "application/json; charset=utf-8",
+           // $.ajax({type: "POST",  url: "http://192.168.0.102:7010/api/articulos/", contentType: "application/json; charset=utf-8",
                 dataType: "json", data:t}).done(function( filasafectadas ) {  
                
                 if (filasafectadas>0)
                 {
-                  
-                  //navigator.notification.alert('Comentario insertado',null,'Guardar comentario','Aceptar');
-				  alert('Comentario insertado');
-				  $("#textbitacora").val(''); 
+                  $('#txt-boleta').val(""); 
+                  $('#txt-articulo').val(""); 
+                  $('#num-cantidad').val(0);
+                  $('#DescripcionArticulo').html("");
+                  existearticulo=0;
+                  alert("Articulo insertado");
                 }
                 else
                 {
-                    //navigator.notification.alert('Error al guardar comentario, contacte al administrador del sistema',null,'Guardar comentario','Aceptar');
-					alert('Error al guardar comentario');
+                    alert("Ocurrio un error al guardar el conteo, consulte al administrador del sistema");
                 }
-				
+
+
+
+            
             }); //AJAX  
 
-		 //navigator.notification.alert('Comentario Ingresado',null,'Guardar Comentario','Aceptar');
-		 
-	 }
-		 
 
 
-	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
- });
- $('#BtnEntregar').click(function () {  
-	 var objetocomentario =  
-                 {            
-                     ClavePedido: pedidoactual,  
-                     Entregado: true          
-                };  
-         
-            var t = JSON.stringify(objetocomentario);              
-            $.ajax({type: "POST",  url: "http://Hergomrdp.ddns.net:7010/api/pedidos/", contentType: "application/json; charset=utf-8",           
-                dataType: "json", data:t}).done(function( filasafectadas ) {  
+
+          /*
+          $.ajax({url: "http://localhost:7010/api/articulos/"+ articulo, success: function(resultado){
+            
+          //alert(resultado[0].Descripcion);
+          
+            $('#DescripcionArticulo').html(resultado[0].Descripcion);
                
-                if (filasafectadas>0)
-                {
-                  
-                  //navigator.notification.alert('Pedido Actualizado',null,'Actualizar pedido','Aceptar');
-				  alert('Pedido Actualizado');
-				  CrearGridPedidos();
-                }
-                else
-                {
-                    //navigator.notification.alert('Error al actualizar pedido',null,'Actualizar pedido','Aceptar');
-					alert('Error al actualizar pedido');
-                }
-				
-            }); //AJAX  
-	
-	
-	//$("#gridpedidos").empty();
-	$("#botones").hide();	
-	
-	//  navigator.notification.alert('Dentro de page',null,'Documentos Pendientes de Cobro','Aceptar');
- });
- $('#BtnSalir').click(function () {  
-	  navigator.app.exitApp();
- });
-});//$(document).ready(function() 
+                 
+            },error : function(xhr, status) {        
+              
+              $('#DescripcionArticulo').html("Articulo no encontrado en el catálogo");
+              
+            }
+          });//AJAX
+*/
+             
+           
+
+
+
+        }//existearticulo
+    });//click
+   
+});//$(document).ready(function()
